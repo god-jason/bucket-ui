6f9f92579d84cc1d935348827f6ad8d89e0a713a
jason <jason@zgwit.com>
2024-11-01 11:34:27 +0800

    js编辑器

--- src/app/pages/table-edit-js/table-edit-js.component.ts
+++ src/app/pages/table-edit-js/table-edit-js.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import {NzCodeEditorComponent} from 'ng-zorro-antd/code-editor';
+import {SmartRequestService} from '@god-jason/smart';
+import {ActivatedRoute} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-table-edit-js',
   standalone: true,
-  imports: [],
+  imports: [
+    NzCodeEditorComponent,
+    FormsModule
+  ],
   templateUrl: './table-edit-js.component.html',
   styleUrl: './table-edit-js.component.scss'
 })
 export class TableEditJsComponent {
 
+  code = ""
+
+  table = ''
+  file = ''
+
+  constructor(private rs: SmartRequestService, private route: ActivatedRoute) {
+
+  }
+
+
+  ngOnInit(): void {
+    this.table = this.route.snapshot.parent?.params['id'];
+    this.file = this.route.snapshot.parent?.queryParams['file'];
+    this.load()
+  }
+
+  load() {
+    this.rs.get(`table/${this.table}/conf/${this.file}`).subscribe(res=>{
+      console.log("get", res)
+      this.code = res
+    })
+  }
+
+  save() {
+    console.log(this.table, "fields", this.code);
+    this.rs.post(`table/${this.table}/conf/${this.file}`, this.code).subscribe(res=>{
+
+    })
+  }
 }
